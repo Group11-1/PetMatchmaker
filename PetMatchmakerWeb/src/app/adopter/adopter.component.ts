4dddecc146871c4e4c92552a8e79a5220f837cc9
NIALP710\tania <tgarlan7@students.kennesaw.edu>
2025-02-20 17:53:17 -0500

    "Admin and Adopter Components"

---
+++ PetMatchmakerWeb/src/app/adopter/adopter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adopter',
+  imports: [],
+  templateUrl: './adopter.component.html',
+  styleUrl: './adopter.component.css'
+})
+export class AdopterComponent {
+
+}
