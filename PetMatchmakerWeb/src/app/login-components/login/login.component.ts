223bc2d431e54c30efb751235d56a7134c0be1c3
NIALP710\tania <tgarlan7@students.kennesaw.edu>
2025-02-21 13:49:18 -0500

    Login & Sign Up Page Frontend Developed

--- PetMatchmakerWeb/src/app/login-components/login/login.component.ts
+++ PetMatchmakerWeb/src/app/login-components/login/login.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  faArrowLeft = faArrowLeft;
 }
--- PetMatchmakerWeb/src/app/login-components/sign-up/sign-up.component.ts
+++ PetMatchmakerWeb/src/app/login-components/sign-up/sign-up.component.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.css'
+  styleUrl: './sign-up.component.css',
 })
 export class SignUpComponent {
-
+  faArrowLeft = faArrowLeft;
 }
