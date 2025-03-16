7f8839c6bf0bac86d77d610622918a6b164cae7f
NIALP710\tania <tgarlan7@students.kennesaw.edu>
2025-03-16 15:18:13 -0400

    TS Logic for loading Pets

--- PetMatchmakerWeb/src/app/pet-lisiting/pet-lisiting.component.ts
+++ PetMatchmakerWeb/src/app/pet-lisiting/pet-lisiting.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PetService } from '../core/services/petfinder.service';
+import { Pet } from '../core/models/pet';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pet-lisiting',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pet-lisiting.component.html',
-  styleUrl: './pet-lisiting.component.css'
+  styleUrl: './pet-lisiting.component.css',
 })
-export class PetLisitingComponent {
+export class PetLisitingComponent implements OnInit {
+  pets: Pet[] = [];
 
+  constructor(private petService: PetService) {}
+
+  ngOnInit(): void {
+    this.loadPets();
+  }
+
+  loadPets(): void {
+    this.petService.getPets().subscribe((response: Pet[]) => {
+      this.pets = response;
+      // console.log(this.pets);
+    });
+  }
 }
