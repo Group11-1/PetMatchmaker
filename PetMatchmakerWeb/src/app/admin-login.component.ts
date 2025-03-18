c5ae511f041cea2f0e93dbdbd24104a9774aba3e
Brandy Wingfield <146140826+Bwingfield1@users.noreply.github.com>
2025-03-17 21:08:42 -0400

    Add files via upload
    -changed the admin login routing from questionnaire to admin-dashboard
    - imported path in app routes

---
+++ PetMatchmakerWeb/src/app/admin-login.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+
+@Component({
+  selector: 'app-admin-login',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './admin-login.component.html',
+  styleUrl: './admin-login.component.css',
+})
+export class AdminLoginComponent {
+  username: string = '';
+  password: string = '';
+
+  isLoggedIn: boolean = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  onLogin() {
+    this.authService.loginAdmin(this.username, this.password).subscribe(
+      (response: any) => {
+        if (response.role_id === 1) {
+          // Save the token
+          this.authService.saveToken(response.token);
+
+          // Store role
+          localStorage.setItem('role', response.role_id.toString());
+
+          // Redirect to the admin dashboard
+          this.router.navigate(['/admin-dashboard']);
+        } else {
+          alert('Access denied! You must be an admin to log in.');
+        }
+      },
+      (error) => {
+        console.error('Admin login failed', error);
+        alert('Invalid credentials!');
+      }
+    );
+  }
+}
